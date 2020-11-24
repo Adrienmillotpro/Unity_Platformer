ca2dab689adaff3027ee39907a415a0cc1e4b469
Nohcim <alexis.michon@outlook.fr>
2020-11-24 18:51:42 +0100

    Import modifiable wall + ld Alexis continue

---
+++ Untiy_Platformer/Assets/My_Stuff/Modifiable_Wall/Bricks_Information.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Bricks_Information : MonoBehaviour
+{
+    [Header("Brick Existence")]
+    public bool HiddenOn_Wall1;
+    public bool HiddenOn_Wall2;
+    public bool HiddenOn_Wall3;
+    public bool HiddenOn_Wall4;
+    public bool HiddenOn_Wall5;
+    public bool HiddenOn_Wall6;
+    public bool HiddenOn_Wall7;
+    public bool HiddenOn_Wall8;
+
+    [Space(3)]
+    [Header("Brick Modification")]
+    public string Condition_Wall1;
+    public string Condition_Wall2;
+    public string Condition_Wall3;
+    public string Condition_Wall4;
+    public string Condition_Wall5;
+    public string Condition_Wall6;
+    public string Condition_Wall7;
+    public string Condition_Wall8;
+
+    [Space(3)]
+    [Header("Brick Specials")]
+    public bool Left_Edge;
+    public bool Right_Edge;
+    public bool Right_Continuation;
+    public bool Left_Continuation;
+    public bool Normal;
+}
---
+++ Untiy_Platformer/Assets/My_Stuff/Modifiable_Wall/Editor/Wall_Editor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Randomizer_Wall))]
+public class Wall_Editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();        
+        Randomizer_Wall Rando_Script = (Randomizer_Wall)target;     
+
+        if (GUILayout.Button("Randomize Bricks")) 
+        {
+            Rando_Script.Randomize_Layout();
+        }
+         GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("<"))
+        {
+            Rando_Script.Left_Pressed_Wall();
+        }
+
+        GUILayout.Button(Rando_Script.Parent.GetChild(0).name.Substring(0, 5));
+
+        if (GUILayout.Button(">"))
+            
+        {
+            Rando_Script.Right_Pressed_Wall();
+        }
+
+
+        GUILayout.EndHorizontal();
+        
+        //--------------------------------------------------------------
+
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("<"))
+        {
+            Rando_Script.Left_Pressed_Type_Wall();
+        }
+
+        GUILayout.Button(Rando_Script.Wall_Type);
+
+        if (GUILayout.Button(">"))
+
+        {
+            Rando_Script.Right_Pressed_Type_Wall();
+        }
+
+
+        GUILayout.EndHorizontal();
+    }
+}
