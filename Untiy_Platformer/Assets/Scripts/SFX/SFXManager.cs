b75bcd61064da1627c6f19bff08ffe1544bec4fd
SomeAvalancheGuy <59919173+SomeAvalancheGuy@users.noreply.github.com>
2020-11-27 22:13:52 +0100

    Music

---
+++ Untiy_Platformer/Assets/Scripts/SFX/SFXManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    private enum SongPhase{
+        Null,
+        First,
+        Second,
+        Third,
+    }
+    public GameObject[] SFXTrigger;
+    public AudioClip[] audioTracks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void ManageTracks(AudioClip audioTrack){
+        
+    }
+}
